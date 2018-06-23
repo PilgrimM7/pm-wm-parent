a92166619b94146118002274e8d57c1b9033538d
PilgrimM <1728521335@qq.com>
2018-06-23 14:30:29 +0800

    WebUtils

---
+++ pm-wm-common/src/main/java/com/pilgrimm/core/util/WebUtils.java
@@ -0,0 +1,13 @@
+package com.pilgrimm.core.util;
+
+public class WebUtils {
+	
+	// 获取项目部署路径
+    public String getRealPath() {
+    	String realPath = Thread.currentThread().getContextClassLoader().getResource("").getPath().substring(1);
+		int len = realPath.indexOf("WEB-INF");
+		realPath = realPath.substring(0, len);
+		return realPath;
+    }
+
+}
