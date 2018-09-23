59bc949a0e978aa381e100d55aa57681c724baa7
PilgrimM <1728521335@qq.com>
2018-09-23 12:59:55 +0800

    iText

--- pm-wm-common/src/main/java/com/pilgrimm/core/util/WebUtils.java
+++ pm-wm-common/src/main/java/com/pilgrimm/core/util/WebUtils.java
@@ -3,7 +3,7 @@ package com.pilgrimm.core.util;
 public class WebUtils {
 	
 	// 获取项目部署路径
-    public String getRealPath() {
+    public static String getRealPath() {
     	String realPath = Thread.currentThread().getContextClassLoader().getResource("").getPath().substring(1);
 		int len = realPath.indexOf("WEB-INF");
 		realPath = realPath.substring(0, len);
--- pm-wm-study/src/main/java/com/pilgrimm/wm/study/jadk/pdf/HtmlToPdfTest.java
+++ pm-wm-study/src/main/java/com/pilgrimm/wm/study/jadk/pdf/HtmlToPdfTest.java
@@ -1,12 +1,18 @@
 package com.pilgrimm.wm.study.jadk.pdf;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 
+import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.tool.xml.XMLWorkerFontProvider;
 import com.itextpdf.tool.xml.XMLWorkerHelper;
 
 public class HtmlToPdfTest {
@@ -17,14 +23,22 @@ public class HtmlToPdfTest {
  
 	/**
 	 * 使用 iText XML Worker实现HTML转PDF
-	 * itextpdf-5.5.6.jar
-	 * @param fileName
-	 * @throws Exception
 	 */
 	public static void parseHtmlToPdf() throws Exception {
-		String htmlFile = "F:/iText/demo.html";
 		String pdfFile = "F:/iText/htmlToPdf.pdf";
-		InputStream htmlFileStream = new FileInputStream(htmlFile);
+
+//		String htmlFile = "D:/GitRepository/pm-wm-parent/pm-wm-study/src/main/webapp/doc/iText/demo.html";
+//		InputStream htmlFileStream = new FileInputStream(htmlFile);
+		
+		StringBuilder html = new StringBuilder("");
+		html.append("<!DOCTYPE html>");
+		html.append("<html>");
+		html.append("<head>");
+		html.append("</head>");
+		html.append("<body>");
+		html.append("<h1>你好！</h1>");
+		html.append("</body>");
+		ByteArrayInputStream htmlStringStream = new ByteArrayInputStream(html.toString().getBytes());
  
 		// 创建一个document对象实例
 		Document document = new Document();
@@ -35,9 +49,27 @@ public class HtmlToPdfTest {
 		// 打开当前的document
 		document.open();
  
-		InputStreamReader isr = new InputStreamReader(htmlFileStream, "UTF-8");
-		XMLWorkerHelper.getInstance().parseXHtml(pdfwriter, document, isr);
+//		InputStreamReader isr = new InputStreamReader(htmlFileStream, "UTF-8");
+		
+//		InputStreamReader isr = new InputStreamReader(htmlStringStream, "UTF-8");
+		XMLWorkerHelper.getInstance().parseXHtml(pdfwriter, document, htmlStringStream, Charset.forName("UTF-8"),
+				new AsianFontProvider());
 		document.close();
+	}
+	
+	public static class AsianFontProvider extends XMLWorkerFontProvider {
+		public Font getFont(final String fontname, final String encoding, final boolean embedded, final float size,
+				final int style, final BaseColor color) {
+			BaseFont bf = null;
+			try {
+				bf = BaseFont.createFont("STSong-Light", "UniGB-UCS2-H", BaseFont.EMBEDDED);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Font font = new Font(bf, size, style, color);
+			font.setColor(color);
+			return font;
+		}
 	}
 
 }
